0f8dcfd5da72f7ee302de740eb4410a61742dbec
simonzilbermann <106626490+simonzilbermann@users.noreply.github.com>
2022-08-06 16:38:40 +0300

    Delete user.js

--- api/v1/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {Reg,Login}=require("../controller/user");
-
-//נגדיר נקודת קצה end point
-//עבור הרשמה והתחברות
-router.post("/reg",Reg);
-router.post("/login",Login);
-
-module.exports=router;
