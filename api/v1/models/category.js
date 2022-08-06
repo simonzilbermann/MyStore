7515c5682002724d7b29f432c1465d189b6769b7
simonzilbermann <106626490+simonzilbermann@users.noreply.github.com>
2022-08-06 16:39:03 +0300

    Delete category.js

--- api/v1/models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.pluralize(null);
-const CategorySchema = mongoose.Schema({
-_id:mongoose.Schema.Types.ObjectId,
-CId:String,
-CName:String
-});
-//בצוא מודל עם שם האוסף בבסיס הנתונים והסכימה הכלולים בו
-module.exports = mongoose.model("Category",CategorySchema)
